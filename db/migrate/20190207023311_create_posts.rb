ecb4e01a07f9df3f78e8c61ed241610024282679
Elise Phan <aliceconnic@gmail.com>
2019-02-07 02:33:11 +0000

    Automatically backed up by Learn

--- db/migrate/20190207023311_create_posts.rb
+++ db/migrate/20190207023311_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :desciption
+      t.text :description
+
       t.timestamps null: false
     end
   end
